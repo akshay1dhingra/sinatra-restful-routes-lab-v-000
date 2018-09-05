6d69506f478dd392d2f272cc6fdbcf09f887b62f
Akshay Dhingra <akshay1dhingra@gmail.com>
2018-09-05 02:56:39 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -49,6 +49,7 @@ end
     @recipe.name = params[:name]
     @recipe.ingredients = params[:ingredients]
     @recipe.cook_time = params[:cook_time]
+    @recipe.save
     redirect to '/recipes/#{@recipe.id}'
   end
 

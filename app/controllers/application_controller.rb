10db7781a93e1ef940daa74f60049465a1c5ac5a
Akshay Dhingra <akshay1dhingra@gmail.com>
2018-09-05 02:52:52 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,10 +22,10 @@ class ApplicationController < Sinatra::Base
     erb :new
   end
 
-  post '/recipes' do 
+  post '/recipes' do
     @recipe = Recipe.create(params)
     redirect to '/recipes/"#{@recipe.id}"'
-  end 
+  end
 
   get '/recipes/:id' do
     #binding.pry
